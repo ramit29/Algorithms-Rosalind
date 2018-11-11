0c010999051fdc471db1486f9ecee4fbfae16759
Ramit B <ramitb@rocketmail.com>
2018-11-11 02:54:06 -0500

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -8,30 +8,6 @@ def insertionsort(list):
             j=j-1
         list[j]=value
 
-
-
-
-
-
-
-
-
-
 a=[7,3,2,4]
 insertionsort(a)
 print(a)
-def insertionSort(alist):
-   for index in range(1,len(alist)):
-
-     currentvalue = alist[index]
-     position = index
-
-     while position>0 and alist[position-1]>currentvalue:
-         alist[position]=alist[position-1]
-         position = position-1
-
-     alist[position]=currentvalue
-
-list = [54,26,93,17,77,31,44,55,20]
-insertionSort(list)
-print(list)
